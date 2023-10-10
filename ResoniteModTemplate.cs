7c41f55dcf6f89b9a25666ede1488a2ec1bbb6b0
Cyro <BlueCyro@protonmail.com>
2023-10-09 20:16:15 -0500

    update name + credits

--- ResoniteModTemplate.cs
+++ ResoniteModTemplate.cs
@@ -9,15 +9,13 @@ namespace Resonance;
 
 public class ResonancePatcher : ResoniteMod
 {
-    public override string Name => "Resonite Mod Template";
-    public override string Author => "You! :)";
+    public override string Name => "Resonance";
+    public override string Author => "Cyro";
     public override string Version => "1.0.0";
     public override string Link => "resonite.com";
 
     public override void OnEngineInit()
     {
-        Harmony harmony = new("net.You.ResoniteModTemplate");
-        var what = Engine.Current.WorldManager.FocusedWorld.LocalUser.LocalUserRoot;
-        float3 huh = what.GetGlobalPosition(UserRoot.UserNode.Head);
+        Harmony harmony = new("net.Cyro.Resonance");
     }
 }
